e77419ce0dc0dd1b0c856b3b0108e2ad1591608c
DILEGENT1 <di.abaki@gmail.com>
2023-03-09 04:47:02 +0400

    Task

--- src/pages/Board/Board.js
+++ src/pages/Board/Board.js
@@ -1,3 +1,4 @@
+import {useState, useEffect} from 'react';
 import Lane from '../../components/Lane/Lane';
 import './Board.css';
 
@@ -9,10 +10,33 @@ const lanes = [
 ];
 
 function Board() {
+  const [loading, setLoading] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  const [error,setError] = useState('');
+  useEffect(()=> {
+    async function fetchData(){
+      try{
+        const tasks = await fetch('https://my-json-server.typicode.com/dile2001/fakejson/tasks');
+        const result = await tasks.json();
+        if(result){
+          setTasks(result);
+          setLoading(false);
+        }
+      }catch(e){
+        setLoading(false);
+        setError(e.message);
+      }
+    }
+    fetchData();
+  },[]);
   return (
     <div className='Board-wrapper'>
       {lanes.map((lane) => (
-        <Lane key={lane.id} title={lane.title} />
+        <Lane key={lane.id} title={lane.title} 
+              loading={loading}
+              error={error}
+              tasks={tasks.filter((task)=> task.lane===lane.id)}
+        />
       ))}
     </div>
   );
